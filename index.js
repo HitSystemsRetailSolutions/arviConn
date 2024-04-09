3390ecdd033bf3f0c10cd44e790e69f3838e449c
Jonatan Funes <jonatanfunesaguera@gmail.com>
2024-04-09 12:56:45 +0200

    Leer csv y meterlo en la base de datos

--- index.js
+++ index.js
@@ -3,14 +3,38 @@ const { Client } = require("basic-ftp");
 const stream = require('stream');
 const { Readable } = require('stream');
 const fs = require('node:fs');
+const { runSql } = require('./sqlService');
 
+let IP = process.env.FTP_HOST;
+let nombreEmpresa;
+let database = 'fac_demo'
+let iteracion = 0;
+let MAXiteracion = 0;
 async function checkForTextInFTP(searchText) {
     const client = new Client();
     client.ftp.verbose = false; // Canviar a true per a debug
-
+    const ipQ = `SELECT * from [Llicencies] where IdExterna = '192.168.1.50';`;
+    
+    const resultIP = await runSql(ipQ, 'Hit');
+    MAXiteracion = resultIP.length - 1;
+    if (resultIP.length > 0) {
+        IP = resultIP[iteracion].IdExterna;
+        nombreEmpresa = resultIP[iteracion].Empresa;
+    }
+    const databaseQ = `select db from [web_empreses] where nom = ${nombreEmpresa}`;
+    const resultDB = await runSql(databaseQ, 'Hit');
+    if (resultDB.length > 0) {
+        database = resultDB[iteracion].db;
+    }
+    /* Coger de la base de datos la IP y la DATABASE donde sincronizo 
+    use Hit;
+    SELECT * from [Llicencies] where IdExterna = '192.168.1.50';
+    select db from [web_empreses] where nom = 'Demo'
+    */
     try {
+        
         await client.access({
-            host: process.env.FTP_HOST,
+            host: IP,
             user: process.env.FTP_USER,
             password: process.env.FTP_PASSWORD,
             secure: false, // Canviar a true si es requereix una connexió segura
@@ -46,8 +70,20 @@ async function checkForTextInFTP(searchText) {
         //        await client.uploadFrom(csvStream, 'Recibo_detalle.csv');
         //        console.log('Fitxer CSV generat i pujat amb èxit.');
         */
+        const fs = require('fs');
+        const readline = require('readline');
+
+        // Ruta al archivo CSV
+        const csvFilePath = './Recibo_detalle.csv';
+
+        // Obtener la fecha de hoy en formato "MM DD YYYY"
+        const today = new Date();
+        const mesActual = (today.getMonth() + 1).toString().padStart(2, "0"); // Obtiene el mes actual en formato de dos dígitos
+        const añoActual = today.getFullYear().toString(); // Obtiene el año actual
+
+        // Crear una interfaz de lectura de archivo
         const fileList = await client.list();
-        process.stdout.write('.')
+        process.stdout.write('.');
         for (const file of fileList) {
             if (file.type === 1 && file.name == 'Recibo_detalle.csv') { // És un fitxer (no un directori)
                 const passThrough = new stream.PassThrough();
@@ -59,18 +95,35 @@ async function checkForTextInFTP(searchText) {
                     content += chunk.toString();
                 });
                 passThrough.on('end', () => {
-                    //                console.log("Contingut: ", content)
+                    //console.log("Contingut: ", content)
                     if (content.includes(searchText)) {
                         console.log("Trobat !!");
                         // No fem return aquí perquè estem dins d'un event listener
                     }
                 });
-                try {
-                    await client.remove('PLULISTADESCARGA_Dwn.csv');
-                } catch (error) {
-                    console.log('There is no "PLULISTADESCARGA_Dwn.csv"')
-                }
+
+                const rl = readline.createInterface({
+                    input: fs.createReadStream(csvFilePath),
+                    output: process.stdout,
+                    terminal: false
+                });
+
+                let lineaActual = null; // Variable para almacenar la línea actual del archivo CSV
+                const lines = [];
+
+                // Evento que se dispara cuando se lee una línea
+                rl.on('line', async (line) => {
+                    lineaActual = line; // Almacenar la línea actual
+                    lines.push(lineaActual)
+                });
+                // Evento que se dispara cuando se termina de leer el archivo
+                rl.on('close', () => {
+                    console.log('Lectura completa del archivo CSV.');
+                    processLines(lines); //Leer todo el array
+                });
+
             }
+
         }
 
     } catch (error) {
@@ -79,42 +132,131 @@ async function checkForTextInFTP(searchText) {
         client.close();
     }
 
-    const fs = require('fs');
-    const readline = require('readline');
-
-    // Ruta al archivo CSV
-    const csvFilePath = './Recibo_detalle.csv';
-
-    // Obtener la fecha de hoy en formato "MM DD YYYY"
-    const today = new Date().toLocaleDateString('en-US', { month: '2-digit', day: '2-digit', year: 'numeric' }).replace(/\//g, ' ');
-
-    // Crear una interfaz de lectura de archivo
-    const rl = readline.createInterface({
-        input: fs.createReadStream(csvFilePath),
-        output: process.stdout,
-        terminal: false
-    });
-
-    // Evento que se dispara cuando se lee una línea
-    rl.on('line', (line) => {
-        // Dividir la línea en campos utilizando la coma como separador
-        const fields = line.split(',');
-
-        // Obtener el valor del campo "DATA"
-        const dataField = fields[0].trim();
-
-        // Comprobar si el valor del campo "DATA" coincide con la fecha de hoy
-        if (dataField === today) {
-            // Imprimir la línea si coincide
-            console.log('Línea para el día de hoy:', line);
-        }
-    });
-
-    // Evento que se dispara cuando se termina de leer el archivo
-    rl.on('close', () => {
-        console.log('Lectura completa del archivo CSV.');
-    });
 }
+
+let acumuladorLinea = '';
+
+async function processLine(line, ultimoTicket) {
+    const today = new Date();
+    const mesActual = (today.getMonth() + 1).toString().padStart(2, "0");
+    const añoActual = today.getFullYear().toString();
+    let mes, dia, año, horas, minutos;
+    // Verificar si la línea actual está vacía o es solo un salto de línea
+    if (line.trim() === '') {
+        // Si es solo un salto de línea, continuar con la siguiente línea
+        return;
+    }
+
+    // Si hay acumulador, significa que estamos uniendo dos líneas
+    if (acumuladorLinea !== '') {
+        // Unir la línea actual con la acumulada
+        line = acumuladorLinea + line;
+        // Limpiar el acumulador
+        acumuladorLinea = '';
+    }
+
+    // Verificar si la línea actual contiene un número impar de comillas
+    // Esto indica que hay un salto de línea dentro de las comillas
+    if ((line.match(/"/g) || []).length % 2 !== 0) {
+        // Guardar el texto de la línea actual en el acumulador
+        acumuladorLinea = line;
+        // No procesar esta línea, esperar a la próxima
+        return;
+    }
+    line = line.replace(/"\n"/g, ' ');
+    const fields = line.split(',');
+
+    // Obtener el valor del campo "DATA"
+    const dataField = fields[0].trim();
+
+    // Obtener el valor del campo "HORA"
+    const horaField = fields[1].trim();
+
+    // Convertir la hora a formato de hora típico
+    let horaNormalizada = horaField;
+
+    // Verificar la longitud de la cadena
+    if (horaField.length === 3) {
+        horas = horaField.substring(0, 1);
+        minutos = horaField.substring(1);
+
+        horaNormalizada = `${horas.padStart(2, '0')}:${minutos.padStart(2, '0')}`;
+    } else if (horaField.length === 4) {
+        horas = horaField.substring(0, 2);
+        minutos = horaField.substring(2);
+
+        horaNormalizada = `${horas}:${minutos.padStart(2, '0')}`;
+    }
+
+    const partes = dataField.split(" ");
+    mes = partes[0].padStart(2, "0");
+    dia = partes[1];
+    año = partes[2];
+
+    const num_tick = fields[2].trim();
+    //Falta meter varios productos con el mismo numero de ticket
+    if (ultimoTicket < num_tick || ultimoTicket == '') {
+        if (mes === mesActual && año === añoActual) {
+            console.log('Línea :', line);
+            const tabla = `[V_Venut_${año}-${mes}]`;
+            const sqlQ = `SELECT top 1 * FROM ${tabla}`;
+            try {
+                const botiga = 1; //No se que poner
+                const dependenta = fields[8].trim();
+                const plu = fields[3].trim();
+                const quantitat = fields[5].trim();
+                const importe = fields[7].trim();
+                const tipus_venta = 'V';
+                const forma_marcar = 'No se'; //No se que poner
+                const otros = 'Testing'; //No se que poner
+                const insertVenuts = `INSERT INTO ${tabla}(Botiga, Data, Dependenta, Num_tick, Estat, Plu, Quantitat, Import, Tipus_venta, FormaMarcar, Otros) VALUES('${botiga}', CONVERT(DATETIME, '${año}-${mes}-${dia} ${horas}:${minutos.padStart(2, '0')}', 120), '${dependenta}', '${num_tick}', '', '${plu}', ${quantitat}, ${importe}, '${tipus_venta}', '${forma_marcar}', '${otros}')`;
+                await runSql(insertVenuts, database);
+                if (num_tick > ultimoTicket)
+                    ultimoTicket = num_tick;
+                //console.log(`Records: ${año} ${mes} ${dia} ${horas} ${minutos}`)
+                //console.time("Patata")
+
+                console.log("--------------------------------------------------------------------------");
+                //console.timeEnd("Patata");
+            } catch (error) {
+                console.error('Error al ejecutar el insert:', error);
+            }
+        }
+    }
+    return ultimoTicket
+}
+
+async function processLines(lines) {
+    const recordsQ = `SELECT * FROM [records] WHERE LEFT(concepte, LEN('Ultimo ticket:')) = 'Ultimo ticket:'`;
+    const resultRecords = await runSql(recordsQ, database);
+    //console.log(resultRecords);
+
+    let ultimoTicket = '';
+    if (resultRecords.length > 0) {
+        const concepte = resultRecords[0].Concepte;
+        ultimoTicket = parseInt(concepte.split(':')[1].trim());
+    }
+    let i = 0
+    for (const line of lines) {
+        //console.log(++i); // Increment and log the index
+        //++i;
+        i = await processLine(line, ultimoTicket);
+    }
+    const recordText = `Ultimo ticket: ${i}`;
+    const records = `INSERT INTO [records] (TimeStamp, Concepte) VALUES(GETDATE(), '${recordText}')`;
+    const updateRecord = `UPDATE [records] SET concepte = '${recordText}' WHERE LEFT(concepte, LEN('Ultimo ticket:')) = 'Ultimo ticket:'`;
+
+    if (i != ultimoTicket) {
+        if (resultRecords.length < 1) {
+            await runSql(records, database);
+            console.log(`Se insertó el registro: '${recordText}'`);
+        } else {
+            await runSql(updateRecord, database);
+            console.log(`Se actualizó el registro existente: '${recordText}'`);
+        }
+    }
+}
+
 
 // Repetir la funció cada 3 segons
 setInterval(() => {
